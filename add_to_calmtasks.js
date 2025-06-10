c3fa1ec621c44efe9db055f9262e900e829d0098
Michael Josephson <30024+mikej@users.noreply.github.com>
2025-06-10 11:06:09 +0100

    update Calmtasks action to use API key

--- add_to_calmtasks.js
+++ add_to_calmtasks.js
@@ -1,8 +1,16 @@
+let credential = Credential.create("Calmtasks");
+credential.addPasswordField("api_key", "API key");
+
+credential.authorize();
+
 let http = HTTP.create();
 
 let response = http.request({
   url: "http://localhost:3000/add/by/text",
   method: "POST",
+  headers: {
+    Authorization: `Bearer ${credential.getValue("api_key")}`,
+  },
   data: {
     text: draft.content,
   },
